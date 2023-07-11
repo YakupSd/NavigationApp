56dedb19ad5aef76f7aa3ec6a84b38b4cc931239
yakupsuda <yakupsuda@gmail.com>
2023-07-11 12:32:22 +0300

    Initial Commit

---
+++ NavigationApp/NavigationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationAppApp.swift
+//  NavigationApp
+//
+//  Created by Yakup Suda on 11.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct NavigationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
